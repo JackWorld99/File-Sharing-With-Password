da9e0b2b556111929f4d74c0281835f4ca6912f6
JackWorld <jackwong0326@gmail.com>
2022-07-12 22:36:14 +0800

    Update File models.

--- models/File.js
+++ models/File.js
@@ -1,10 +1,20 @@
 const mongoose = require("mongoose");
 
 const File = new mongoose.Schema({
-  path: { typr: String, required: true },
-  originalName: { type: String, required: true },
+  path: {
+    type: String,
+    required: true,
+  },
+  originalName: {
+    type: String,
+    required: true,
+  },
   password: String,
-  downloadCount: { type: Number, required: true, default: 0 },
+  downloadCount: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
 });
 
 module.exports = mongoose.model("File", File);
