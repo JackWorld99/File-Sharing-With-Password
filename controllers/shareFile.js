83489ffc04a6ed575fe3fa859b773ed0078261c0
JackWorld <jackwong0326@gmail.com>
2022-07-13 17:58:07 +0800

    Update with MVC architectural.

---
+++ controllers/shareFile.js
@@ -0,0 +1,42 @@
+const bcrypt = require("bcrypt");
+const File = require("../models/File");
+
+exports.index = (req, res) => {
+  res.render("index");
+};
+
+exports.uploadFile = async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname,
+  };
+
+  if (req.body.password != null && req.body.password !== "") {
+    fileData.password = await bcrypt.hash(req.body.password, 10);
+  }
+
+  const file = await File.create(fileData);
+
+  res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
+};
+
+exports.handleDownload = async (req, res) => {
+  const file = await File.findById(req.params.id);
+
+  if (file.password != null) {
+    if (req.body.password == null) {
+      res.render("download");
+      return;
+    }
+
+    if (!(await bcrypt.compare(req.body.password, file.password))) {
+      res.render("download", { error: true });
+      return;
+    }
+  }
+
+  file.downloadCount++;
+  await file.save();
+
+  res.download(file.path, file.originalName);
+};
--- server.js
+++ server.js
@@ -1,62 +1,28 @@
-require("dotenv").config();
-const multer = require("multer");
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const File = require("./models/File");
-
 const express = require("express");
-const app = express();
-app.use(express.urlencoded({extended:true}))
-
+const mongoose = require("mongoose");
+const urlRoute = require("./routes/index");
+require("dotenv").config();
 const port = process.env.PORT || 3000;
 
-const upload = multer({ dest: "uploads" });
+const app = express();
 
-mongoose.connect(process.env.DATABASE_URL);
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Databse Connected Successfully!");
+  })
+  .catch((error) => {
+    console.log("Could not connect to database", error);
+    process.exit();
+  });
 
 app.set("view engine", "ejs");
-
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.post("/upload", upload.single("file"), async (req, res) => {
-  const fileData = {
-    path: req.file.path,
-    originalName: req.file.originalname,
-  };
-
-  if (req.body.password != null && req.body.password !== "") {
-    fileData.password = await bcrypt.hash(req.body.password, 10);
-  }
-
-  const file = await File.create(fileData);
-
-  res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` });
-});
-
-app.route("/file/:id").get(handleDownload).post(handleDownload)
-
-async function handleDownload(req, res){
-  const file =await File.findById(req.params.id)
-
-    if(file.password!=null){
-        if(req.body.password == null){
-            res.render("download")
-            return
-        } 
-
-        if(!(await bcrypt.compare(req.body.password, file.password))){
-          res.render("download", { error: true })
-          return
-        }
-    }
-
-    file.downloadCount++;
-    await file.save()
-
-    res.download(file.path,file.originalName)
-}
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use("/", urlRoute);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
